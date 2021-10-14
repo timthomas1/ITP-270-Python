46514e3c02d6b4a462fd02a38783757e3f5b8c97
Timothy L. Thomas <thomas.tlt94@gmail.com>
2021-10-14 17:42:27 -0400

    Add files via upload

---
+++ monthlyReconcile.java
@@ -0,0 +1,43 @@
+//This program is make sure Person1 and Person2 pay an equal share for shared bills.
+
+package monthlyReconcile;
+
+import java.util.Scanner;
+
+import java.text.NumberFormat;
+
+public class monthlyReconcile {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		NumberFormat money = NumberFormat.getCurrencyInstance();
+		double timPay;
+		double yosPay;
+		double monthlyTotal;
+		double equalPay;
+		double timShare;
+		double yosShare;
+		
+		System.out.print("How much did Tim pay? $");
+		timPay = input.nextDouble();
+		
+		System.out.print("How much did Yosmary pay? $");
+		yosPay = input.nextDouble();
+		
+		monthlyTotal = timPay + yosPay;
+		
+		equalPay = monthlyTotal / 2;
+		
+		timShare = timPay - equalPay;
+		yosShare = yosPay - equalPay;
+
+		if (timPay > yosPay) {
+			System.out.println("Yosmary owes Tim " + money.format(timShare));
+		}
+		
+		else if (timPay < yosPay) {
+			System.out.println("Tim owes Yosmary " + money.format(yosShare));
+		}
+	}
+}
